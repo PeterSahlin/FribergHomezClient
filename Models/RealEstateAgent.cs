7ab7d86f2ecf390d6390cfc8689cd79be317eebf
PeterSahlin <145502083+PeterSahlin@users.noreply.github.com>
2024-04-24 15:01:42 +0200

    Added page for real estate agent to view their listings.

--- Models/RealEstateAgent.cs
+++ Models/RealEstateAgent.cs
@@ -2,8 +2,19 @@
 {
     public class RealEstateAgent
     {
+        public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
+
+        public string FullName
+        {
+            get
+            {
+                return FirstName +" "+ LastName;
+            }
+            set { }
+        }
+        
         public string Email { get; set; }
         public string PhoneNumber { get; set; }
         public string ImageUrl { get; set; }
