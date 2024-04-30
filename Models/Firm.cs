bf0ee4d0516dd94c3b7f87dffcb3ab35c4d89196
PeterSahlin <145502083+PeterSahlin@users.noreply.github.com>
2024-04-30 16:19:37 +0200

    Added all Real estate stuff

--- Models/Firm.cs
+++ Models/Firm.cs
@@ -2,6 +2,7 @@
 {
     public class Firm
     {
+        public int Id { get; set; }
         public string Name { get; set; } = "";
         public string Presentation { get; set; } = "";
         public string ImageLocation { get; set; } = "";
