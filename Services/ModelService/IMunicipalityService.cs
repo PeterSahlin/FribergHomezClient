c0ff0b34a4a635d091bce0f6adf64b47ba1dd4f4
Thomas Sahlin <thomassahlin@me.com>
2024-05-17 15:52:07 +0200

    Added services for municipalities and categories

---
+++ Services/ModelService/IMunicipalityService.cs
@@ -0,0 +1,19 @@
+﻿using FribergHomezClient.Services.Base;
+
+namespace FribergHomezClient.Services.ModelService
+{
+    public interface IMunicipalityService
+    {
+        Task<Response<List<Municipality>>> GetMunicipalitiesAsync();
+
+        Task<Response<Municipality>> GetMunicipalityByIdAsync(int municipalityid);
+
+        Task CreateMunicipalityAsync(Municipality municipality);
+
+        Task<Response<Municipality>> DeleteMunicipalityAsync(int municipalityid);
+
+        Task UpdateMunicipalityAsync(Municipality municipality);
+
+
+    }
+}
