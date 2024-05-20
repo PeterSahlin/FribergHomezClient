aec072d59e358794415618dd992c9c57f82c9fb9
PeterSahlin <145502083+PeterSahlin@users.noreply.github.com>
2024-05-20 12:01:29 +0200

    Styling for saleobjects

--- Models/AgentDTO.cs
+++ Models/AgentDTO.cs
@@ -7,7 +7,7 @@
 
         public string? Email { get; set; }
         public string? PhoneNumber { get; set; }
-        public string ImageUrl { get; set; }
+        public string ImageUrl { get; set; } = "https://www.pngfind.com/pngs/m/610-6104451_image-placeholder-png-user-profile-placeholder-image-png.png";
         public int? FirmId { get; set; }
         public bool IsActive { get; set; } = true;
         public string Password { get; set; }
